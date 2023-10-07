5b3c808d35f24cdf5b65b7c081df13bd7b8e514f
Muniunga <muniunga2000@gmail.com>
2023-10-07 09:47:56 +0100

    sábado

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
 const app = Vue.createApp({
-    data (){
+    data() {
         return {
-            name:'',
+            name: '',
             login: '',
             location: '',
-            company:'',
-            avatar_url:'',
-            searchInput:'',
+            company: '',
+            avatar_url: '',
+            searchInput: '',
         }
     },
     methods: {
-       async fetchGitHubeUser(){
-            const res= await fetch('https://api.github.com/users/${this.searchInput}')
-            const { login, name, location, company, avatar_url}= await res.json()
-            this.name= name
-            this.login= login
-            this.location= bilocationo
-            this.company= company
-            this.avatar_url= avatar_url
+        async fetchGitHubUser() { 
+            const res = await fetch(`https://api.github.com/users/${this.searchInput}`)
+            const { login, name, location, company, avatar_url } = await res.json()
+            this.name = name
+            this.login = login
+            this.location = location 
+            this.company = company
+            this.avatar_url = avatar_url
         }
     },
 })
+
 app.mount('#app')
